a5482d693ac662398096c95195743e31ca2305ce
S1mPLe756 <gaz.radik11@mail.ru>
2025-12-09 00:23:00 +0300

    Fix bug with invite

--- src/usecases/workspace/canChangeWorkspace.js
+++ src/usecases/workspace/canChangeWorkspace.js
@@ -1,5 +1,5 @@
 import {workspaceService} from "../../features/workspace/workspaceService";
 
 export const CanChangeWorkspace = async (workspaceId) => {
-  await workspaceService.canChange(workspaceId);
+  return await workspaceService.canChange(workspaceId);
 };
